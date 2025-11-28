b1dbf4ed55040e8433a0db58f3622421b27ff357
roshankshatriya <roshankshatriya75@gmail.com>
2025-11-28 13:57:21 +0530

    pagination page added

---
+++ app/products/page.tsx
@@ -0,0 +1,66 @@
+'use client'
+
+import React, { useEffect, useState } from 'react'
+
+const PAGE_SIZE = 10;
+
+const Products = () => {
+
+    const [products, setProducts] = useState([]);
+    const [currentPage, setCurrentPage] = useState(0);
+
+    const fetchProducts = async () => {
+        const data = await fetch('https://dummyjson.com/products?limit=154');
+        const json = await data.json();
+        setProducts(json.products);
+    }
+
+    useEffect(() => {
+        fetchProducts();
+    }, []);
+
+    const totalProducts = products.length;
+    const totalPages = Math.ceil(totalProducts / PAGE_SIZE);
+    const start = currentPage * PAGE_SIZE;
+    const end = start + PAGE_SIZE;
+
+    const handlePageChange =(n)=> {
+        setCurrentPage(n);
+    }
+
+
+  return (
+    <>
+        <h1 className='text-2xl text-center pt-5'>All Products</h1>
+
+        <div className='products-container'>
+            {products.slice(start, end).map((products:any)=>(
+                <div key={products.id} className='product-card'>
+                    <h2>{products.title}</h2>
+                    <img src={products.thumbnail} alt={products.title} width={200} height={200} />
+                    {/* <p>{products.description}</p> */}
+                    <p className='font-semibold'>Price: ${products.price}</p> 
+                    
+
+                </div>
+                
+            ))}
+        </div>
+        <div className='pagination'>
+            {[...Array(totalPages).keys()].map((n) => (
+                <span
+                key={n}
+                onClick={() => handlePageChange(n)}
+                className={`page-number ${n === currentPage ? 'active-page' : ''}`}
+                >
+                {n + 1}
+                </span>
+            ))}
+        </div>
+
+    </>
+
+  )
+}
+
+export default Products;
